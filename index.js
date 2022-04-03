f91a32f32a33d38d9bd2b8ebc237218cd3fe4d1b
Alan Verdugo <factorpolar@protonmail.com>
2022-04-03 18:10:07 -0500

    Testing

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-console.log("running test!");
+const Runner = require('./runner');
+
+const runner = new Runner();
+
+const run = async () => {
+    await runner.collectFiles(process.cwd());
+    runner.runTest();
+};
+
+run();
